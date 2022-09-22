919acd903a1c871319c6ec27d6eabe81131d3e42
Sulas <>
2022-09-22 15:15:50 +0000

    Completar gerenciador de aeronaves

--- src/pucrs/myflight/consoleApp/App.java
+++ src/pucrs/myflight/consoleApp/App.java
@@ -4,9 +4,6 @@ public class App {
 
 	public static void main(String[] args) {
 		System.out.println("\nMyFlight project...");
-		//aaaaa8778877878
-// Alterado no github
-// Aterado no VScode
-// Criando e publicando uma nova branch
+
 	}
 }
--- src/pucrs/myflight/modelo/GerenciadorAeronaves.java
+++ src/pucrs/myflight/modelo/GerenciadorAeronaves.java
@@ -1,5 +1,28 @@
 package pucrs.myflight.modelo;
 
-public class GerenciadorAeronaves {
+import java.util.ArrayList;
 
+public class GerenciadorAeronaves {
+    private ArrayList<Aeronave> aeronaves;
+
+    public GerenciadorAeronaves() {
+        aeronaves = new ArrayList<>();
+    }
+    
+    public void adiciona(Aeronave aviao) {
+        aeronaves.add(aviao);
+    }
+
+    public ArrayList<Aeronave> listarTodas() { //TODO
+        return null;
+    }
+
+    public Aeronave buscarPorCodigo(String cod) {
+        for (Aeronave a : aeronaves) {
+            if (a.getCodigo().equals(cod)) {
+                return a;
+            }
+        }
+        return null;
+    }
 }
