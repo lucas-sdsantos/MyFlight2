765c88e30cee4b83ff5ec6ad2c6cc85af1baf4d9
Sulas <97130387+schwartzlucas@users.noreply.github.com>
2022-09-22 15:46:12 +0000

    GerenciadorVoos

--- src/pucrs/myflight/modelo/GerenciadorAeroportos.java
+++ src/pucrs/myflight/modelo/GerenciadorAeroportos.java
@@ -3,4 +3,21 @@ package pucrs.myflight.modelo;
 import java.util.ArrayList;
 
 public class GerenciadorAeroportos {
+    private ArrayList<Aeroporto> aeroportos;
+    
+    GerenciadorAeroportos() {
+        aeroportos = new ArrayList<>();
+    }
+
+    public void adicionar(Aeroporto aero) { //TODO
+        
+    }
+
+    public ArrayList<Aeroporto> listarTodos() { //TODO
+        
+    }
+
+    public Aeroporto buscarPorCodigo(String cod) { //TODO
+        
+    }
 }
--- src/pucrs/myflight/modelo/GerenciadorVoos.java
+++ src/pucrs/myflight/modelo/GerenciadorVoos.java
@@ -1,5 +1,21 @@
 package pucrs.myflight.modelo;
 
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+
 public class GerenciadorVoos {
 
+    private ArrayList<Voo> voos;
+    
+    GerenciadorVoos() {
+        voos = new ArrayList<>();
+    }
+
+    public ArrayList<Voo> listarTodos() { //TODO
+        
+    }
+
+    public ArrayList<Voo> buscarData(LocalDate date) { //TODO
+        
+    }
 }
