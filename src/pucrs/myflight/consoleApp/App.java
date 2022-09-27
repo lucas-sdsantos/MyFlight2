799dff0db3f864c4453a13f2af6225eafa126f40
Rationapila <108445811+Rationapila@users.noreply.github.com>
2022-09-27 17:07:33 -0300

    Construtor alternativo de Voo feito e testado na main

--- src/pucrs/myflight/consoleApp/App.java
+++ src/pucrs/myflight/consoleApp/App.java
@@ -1,11 +1,12 @@
 package pucrs.myflight.consoleApp;
-
+import java.time.Duration;
+import java.time.LocalDateTime;
 import pucrs.myflight.modelo.*;
 
 public class App {
 
 	public static void main(String[] args) {
-		Aeronave a = new Aeronave("13", "PT", 13);
+		Aeronave a = new Aeronave("13", "PT");
 		System.out.println(a);
 		Geo b = new Geo(3, 0);
 		System.out.println(b);
@@ -14,6 +15,18 @@ public class App {
 		CiaAerea d = new CiaAerea("22", "PL");
 		System.out.println(d);
 
+		CiaAerea ciaA = new CiaAerea ("34534534355", "Gol");
+		Geo geoA = new Geo (172.1987, -67.6805);
+		Geo geoB = new Geo (74.0824, -102.5554);
+		Aeroporto aeroportoA = new Aeroporto ("87994", "Aeroporto Salgado Filho", geoA);
+		Aeroporto aeroportoB = new Aeroporto ("60047", "Aeroporto Caxias do Sul", geoA);
+		Aeronave aeronaveA = new Aeronave ("9566543245", "eVTOL");
+		Rota rotaA = new Rota (ciaA, aeroportoA, aeroportoB, aeronaveA);
+		LocalDateTime localDateTimeA = LocalDateTime.of(2022, 9, 27, 16, 57);
+		Duration durationA = Duration.ofMinutes(120);
+		//Teste para verificar a funcionalidade dos dois construtores.
+		Voo vooA = new Voo(rotaA, localDateTimeA, durationA);
+		Voo vooB = new Voo(rotaA, durationA);
 		
 	}
 }
