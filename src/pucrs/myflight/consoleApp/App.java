6b2e40f2575d5a3da117c3b70128265a98e12509
Lucas Schwartz <schwartzlucas.santos@gmail.com>
2022-09-29 17:45:37 -0300

    Teste dos metodos da classe CiaAerea

--- src/pucrs/myflight/consoleApp/App.java
+++ src/pucrs/myflight/consoleApp/App.java
@@ -16,22 +16,29 @@ public class App {
 
 	public static void main(String[] args) {
 
+		// Teste do construtor da classe CiaAerea
 		CiaAerea ciaA = new CiaAerea("34534534355", "Gol");
-		System.out.println(ciaA);
+		System.out.println(ciaA.getCodigo());
+		System.out.println(ciaA.getNome());
 
+		// Tesde do construtor da classe Geo
 		Geo geoA = new Geo(172.1987, -67.6805);
 		Geo geoB = new Geo(74.0824, -102.5554);
-		System.out.println(geoB);
+		System.out.println(geoA);
 
+		// Teste do construtor da classe Aeroporto
 		Aeroporto aeroportoA = new Aeroporto("87994", "Aeroporto Salgado Filho", geoA);
 		Aeroporto aeroportoB = new Aeroporto("60047", "Aeroporto Caxias do Sul", geoA);
 		System.out.println(aeroportoA);
 
+		// Teste do construtor de classe Aeronave
 		Aeronave aeronaveA = new Aeronave("9566543245", "eVTOL");
 		System.out.println(aeronaveA);
 
+		// Teste do construtor da classe Rota
 		Rota rotaA = new Rota(ciaA, aeroportoA, aeroportoB, aeronaveA);
 		System.out.println(rotaA);
+
 
 		LocalDateTime localDateTimeA = LocalDateTime.of(2022, 9, 27, 16, 57);
 		System.out.println(localDateTimeA);
