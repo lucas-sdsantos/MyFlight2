28e0e355fccd9acb9a5de2824ba3cfde421a4ddc
Lucas Schwartz <schwartzlucas.santos@gmail.com>
2022-09-29 18:58:45 -0300

    Teste VooDireto

--- src/pucrs/myflight/consoleApp/App.java
+++ src/pucrs/myflight/consoleApp/App.java
@@ -2,6 +2,7 @@ package pucrs.myflight.consoleApp;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.temporal.TemporalUnit;
 
 import pucrs.myflight.modelo.Aeronave;
 import pucrs.myflight.modelo.Aeroporto;
@@ -24,6 +25,7 @@ public class App {
 		// Teste dos métodos da classe Geo
 		Geo geoA = new Geo(172.1987, -67.6805);
 		Geo geoB = new Geo(74.0824, -102.5554);
+		Geo geoC = new Geo(-83.5016, -22.5664);
 		System.out.println(geoA.getLatitude());
 		System.out.println(geoA.getLongitude());
 			// Calcular a distância entre dois pontos.
@@ -34,6 +36,7 @@ public class App {
 		// Teste dos métodos da classe Aeroporto
 		Aeroporto aeroportoA = new Aeroporto("87994", "Aeroporto Salgado Filho", geoA);
 		Aeroporto aeroportoB = new Aeroporto("60047", "Aeroporto Caxias do Sul", geoB);
+		Aeroporto aeroportoC = new Aeroporto("48897", "Aeroporto de Guarulhos", geoC);
 		System.out.println("Cod.: " + aeroportoA.getCodigo() + "; Aeroporto: "
 		 + aeroportoA.getNome());
 		System.out.println(aeroportoA.getLocal().getLatitude() + "; " + 
@@ -46,20 +49,19 @@ public class App {
 
 		// Teste dos métodos da classe Rota
 		Rota rotaA = new Rota(ciaA, aeroportoA, aeroportoB, aeronaveA);
+		Rota rotaB = new Rota(ciaA, aeroportoB, aeroportoC, aeronaveA);
 		System.out.println("Empresa: " + ciaA.getNome());
 		System.out.println("Partida: " + aeroportoA.getNome());
 		System.out.println("Destino: " + aeroportoB.getNome());
 		System.out.println("Cod. Aeronave: " + ciaA.getCodigo());
 
 		LocalDateTime localDateTimeA = LocalDateTime.of(2022, 9, 27, 16, 57);
-		System.out.println(localDateTimeA);
-		
-		Duration durationA = Duration.ofMinutes(120);
-		
+		//System.out.println(localDateTimeA);
+
 		// Teste para verificar a funcionalidade dos dois construtores.
 		Voo vooA = new VooDireto(rotaA, localDateTimeA);
-		Voo vooB = new VooEscalas(rotaA, localDateTimeA);
-		System.out.println(vooA);
+		Voo vooB = new VooEscalas(localDateTimeA);
+		System.out.println(vooA.getDuracao().toMinutes());
 		System.out.println(vooB);
 
 	}
